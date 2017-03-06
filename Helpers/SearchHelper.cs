db05904c25fbe287b45f4c20f3878625d8b3058a
Philipp Baum <baum@webix.de>
2017-03-06 17:15:05 +0100

    Added Search

--- Helpers/SearchHelper.cs
+++ Helpers/SearchHelper.cs
@@ -1,34 +1,67 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using WikiCore.DB;
 
 namespace WikiCore.SearchHelpers
 {
-    public static class SearchHelper {
-        public static List<SearchResult> Search(string text) {
+    public static class SearchHelper
+    {
+
+        private const int descriptionWidth = 30;
+        public static List<SearchResult> Search(string searchText)
+        {
             List<SearchResult> result = new List<SearchResult>();
 
-            List<Page> findResults;
+            List<Page> pagesFound;
             using (var db = new WikiContext())
             {
-                findResults = db.Pages.Where(x => x.Content.ToLower().Contains(text) || x.Title.ToLower().Contains(text)).ToList();
+                pagesFound = db.Pages.Where(x => x.Content.ToLower().Contains(searchText) || x.Title.ToLower().Contains(searchText)).ToList();
             }
 
-            foreach (Page item in findResults) {
-               result.Add(new SearchResult{ title = item.Title, url = "/Home/Index/" + item.Id, description = "asdasd"});                 
+            foreach (Page item in pagesFound)
+            {
+                result.Add(new SearchResult { title = item.Title, url = "/Home/Index/" + item.Id, description = GetDescription(searchText, item.Content) });
             }
-
-            //search test data
-            // result.Add(new SearchResult{ title = "test1", url = "google,de", description = "asdasd"});
-            // result.Add(new SearchResult{ title = "testasdasd1", url = "google,de", description = "asdasd"});
-            // result.Add(new SearchResult{ title = "tesasdasdt1", url = "google,de", description = "asdasd"});
-            // result.Add(new SearchResult{ title = "tesasdasdasdasdt1", url = "google,de", description = "asdasd"});
 
             return result;
         }
+
+        private static string GetDescription(string searchText, string content)
+        {
+            int indexOfsearchText = content.IndexOf(searchText);
+            content = content.Replace("\r","").Replace("\n","");
+
+            //If there is les than descriptionWidth-characters infront of the foundText, display from the start
+            int startIndex;
+            if (indexOfsearchText < descriptionWidth)
+            {
+                startIndex = 0;
+            }
+            else
+            {
+                startIndex = indexOfsearchText - descriptionWidth;
+            }
+
+            //Get descLength, if smaller than chars left, get everything
+            int descLength;
+            if (content.Length - descriptionWidth - searchText.Length - startIndex < descriptionWidth)
+            {
+                descLength = content.Length - startIndex;
+            }
+            else
+            {
+                descLength = descriptionWidth + searchText.Length + descriptionWidth;
+            }
+
+            string desc = content.Substring(startIndex, descLength);
+
+            return desc;
+        }
     }
 
-    public class SearchResult {
+    public class SearchResult
+    {
         public string title { get; set; }
         public string url { get; set; }
         public string description { get; set; }
