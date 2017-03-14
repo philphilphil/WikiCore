407b031edba5b000cc215ccf9e630b661bdbfcd5
Philipp Baum <phil@phib.io>
2017-03-14 21:28:45 +0100

    Added delition fo tags

--- Database/DBService.cs
+++ Database/DBService.cs
@@ -26,80 +26,94 @@ namespace WikiCore.DB
             }
         }
 
-        private static void CreateTags(int pageId, string tags)
+        internal static void DeleteTag(int tagId)
         {
             using (var db = new WikiContext())
             {
-                //Todo: Remove Tags that where removed in the view.
+                var tag = db.Tags.Where(t => t.TagId == tagId).FirstOrDefault();
+                db.Tags.Remove(tag);
+                
+                var tagReferences = db.PageTags.Where(p => p.TagId == tagId).ToList();
+                db.PageTags.RemoveRange(tagReferences);
 
-                List<String> tagList = tags.Split(',').ToList();
-
-                foreach (string tag in tagList)
-                {
-                    Tag dbTag = GetTag(tag);
-
-                    CreatePageTageReference(dbTag.TagId, pageId);
-                }
-
+                db.SaveChanges();
             }
         }
 
-        internal static string LoadTags(int pageId)
+    private static void CreateTags(int pageId, string tags)
+    {
+        using (var db = new WikiContext())
         {
-            string pageTags = "";
-            using (var db = new WikiContext())
+            //Todo: Remove Tags that where removed in the view.
+
+            List<String> tagList = tags.Split(',').ToList();
+
+            foreach (string tag in tagList)
             {
-                var allTags = db.PageTags.Where(t => t.PageId == pageId).Select(t => t.Tag).ToList();
+                Tag dbTag = GetTag(tag);
 
-
-                foreach (Tag tag in allTags)
-                {
-                    pageTags += tag.Name + ",";
-                }
-
+                CreatePageTageReference(dbTag.TagId, pageId);
             }
 
-            return pageTags;
         }
+    }
 
-
-        private static void CreatePageTageReference(int tagId, int pageId)
+    internal static string LoadTags(int pageId)
+    {
+        string pageTags = "";
+        using (var db = new WikiContext())
         {
-            using (var db = new WikiContext())
-            {
-                PageTag tag = db.PageTags.Where(t => t.PageId == pageId && t.TagId == tagId).FirstOrDefault();
+            var allTags = db.PageTags.Where(t => t.PageId == pageId).Select(t => t.Tag).ToList();
 
-                if (tag == null)
-                {
-                    PageTag newTag = new PageTag();
-                    newTag.PageId = pageId;
-                    newTag.TagId = tagId;
-                    db.PageTags.Add(newTag);
-                    db.SaveChanges();
-                }
+
+            foreach (Tag tag in allTags)
+            {
+                pageTags += tag.Name + ",";
             }
+
         }
 
-        private static Tag GetTag(string name)
-        {
-            using (var db = new WikiContext())
-            {
-                Tag tag = db.Tags.Where(t => t.Name == name).FirstOrDefault();
+        return pageTags;
+    }
 
-                if (tag == null)
-                {
-                    Tag newTag = new Tag();
-                    newTag.Name = name;
-                    db.Tags.Add(newTag);
-                    db.SaveChanges();
-                    return newTag;
-                }
-                else
-                {
-                    return tag;
-                }
+
+    private static void CreatePageTageReference(int tagId, int pageId)
+    {
+        using (var db = new WikiContext())
+        {
+            PageTag tag = db.PageTags.Where(t => t.PageId == pageId && t.TagId == tagId).FirstOrDefault();
+
+            if (tag == null)
+            {
+                PageTag newTag = new PageTag();
+                newTag.PageId = pageId;
+                newTag.TagId = tagId;
+                db.PageTags.Add(newTag);
+                db.SaveChanges();
             }
         }
     }
+
+    private static Tag GetTag(string name)
+    {
+        using (var db = new WikiContext())
+        {
+            Tag tag = db.Tags.Where(t => t.Name == name).FirstOrDefault();
+
+            if (tag == null)
+            {
+                Tag newTag = new Tag();
+                newTag.Name = name;
+                db.Tags.Add(newTag);
+                db.SaveChanges();
+                return newTag;
+            }
+            else
+            {
+                return tag;
+            }
+        }
+    }
+}
 
 }
