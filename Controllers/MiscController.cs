5c4af8f8c4d2f162ae914915a53c64e833deb95b
Philipp Baum <phil@phib.io>
2017-03-14 21:28:45 +0100

    Added deleton of tags

--- Controllers/MiscController.cs
+++ Controllers/MiscController.cs
@@ -11,11 +11,18 @@ namespace WikiCore.Controllers
             return View(new MiscModel());
         }
 
-        private IActionResult Error(string message) {
+        private IActionResult Error(string message)
+        {
 
             ViewData["ErrorMessage"] = message;
 
             return View("Index", new MiscModel());
+        }
+
+        public IActionResult DeleteTag(MiscModel m)
+        {
+            DBService.DeleteTag(m.TagId);
+            return RedirectToAction("Index");
         }
     }
 }
