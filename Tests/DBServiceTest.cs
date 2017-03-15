26fcf6b2fdb97d1f7cb5387fc05abe99b49fb204
Philipp Baum <baum@webix.de>
2017-03-15 10:59:29 +0100

    Added empty unit test ans xunit

---
+++ Tests/DBServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Wikicore
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
